7cdc7ff0ca078ebb72056c850f3927a8d913c8fe
Muhammad Mostafa <mohamedmoustafaa1998@gmail.com>
2025-02-28 00:35:08 +0200

    Lecture 1: Create Model/Views/Controller for Category

---
+++ EgyNews/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using EgyNews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgyNews.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            Category category = new Category();
+            category.Id = 1;
+            category.Name = "Category 1";
+
+
+            return View(category);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ EgyNews/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace EgyNews.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}

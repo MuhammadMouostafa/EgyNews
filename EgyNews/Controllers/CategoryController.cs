8e748698bf5cbae4b6ffcbd5ce6f2acde2e2e834
Muhammad Mostafa <mohamedmoustafaa1998@gmail.com>
2025-02-28 03:51:34 +0200

    Lecture 2: add CRUD Operations for Category

--- EgyNews/Controllers/CategoryController.cs
+++ EgyNews/Controllers/CategoryController.cs
@@ -1,27 +1,67 @@
-﻿using EgyNews.Models;
+﻿using EgyNews.Data;
+using EgyNews.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EgyNews.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.ToList();
+            return View(categories);
         }
         public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Create(Category newCategory)
         {
-            return View();
+            _db.Categories.Add(newCategory);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete()
+        public IActionResult Edit(int id)
         {
-            return View();
+            var category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
